53111d47575592ff4b36cb2dadc1aa337fe14151
Pancu <pancu1222@gmail.com>
2021-05-03 22:21:51 +0200

    update create_tables

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,12 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def create_database():
     """
-    - Creates and connects to the sparkifydb
-    - Returns the connection and cursor to sparkifydb
+    Establishes database connection and return's the connection and cursor references.
+    :return: return's (cur, conn) a cursor and connection reference
     """
-    
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=root")
+    #conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=udacity user=postgres password=root")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
@@ -29,7 +29,9 @@ def create_database():
 
 def drop_tables(cur, conn):
     """
-    Drops each table using the queries in `drop_table_queries` list.
+    Run's all the drop table queries defined in sql_queries.py
+    :param cur: cursor to the database
+    :param conn: database connection reference
     """
     for query in drop_table_queries:
         cur.execute(query)
@@ -38,7 +40,9 @@ def drop_tables(cur, conn):
 
 def create_tables(cur, conn):
     """
-    Creates each table using the queries in `create_table_queries` list. 
+    Run's all the create table queries defined in sql_queries.py
+    :param cur: cursor to the database
+    :param conn: database connection reference
     """
     for query in create_table_queries:
         cur.execute(query)
@@ -47,21 +51,15 @@ def create_tables(cur, conn):
 
 def main():
     """
-    - Drops (if exists) and Creates the sparkify database. 
-    
-    - Establishes connection with the sparkify database and gets
-    cursor to it.  
-    
-    - Drops all the tables.  
-    
-    - Creates all tables needed. 
-    
-    - Finally, closes the connection. 
+    Driver main function.
     """
     cur, conn = create_database()
     
     drop_tables(cur, conn)
+    print("Table dropped successfully!!")
+
     create_tables(cur, conn)
+    print("Table created successfully!!")
 
     conn.close()
 
